5f0fc7dc36e49001f851578b9b0eff1d6b36644f
Jake Gussler <jakegussler@gmail.com>
2025-01-04 22:56:31 -0500

    create file

---
+++ scripts/create_virtual_environment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Get environment variables from .env file
+ENV_FILE="${1:-../.env}"
+
+if [ -f "$ENV_FILE" ]; then
+    echo "Retrieving variables from $ENV_FILE"
+    source "$ENV_FILE"
+else
+    echo ".env file not found in $ENV_FILE, exiting"
+    exit 1
+fi
+
+cd ${PROJECT_PATH}
+
+# Create a virtual environment named 'myenv'
+python -m venv venv
+
+source venv/bin/activate
+
+# Windows
+#source myenv/Scripts/activate
