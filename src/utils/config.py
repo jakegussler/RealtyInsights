6e329b9eb14455089943a84636c016697e21621c
Jake Gussler <jakegussler@gmail.com>
2025-01-05 13:58:21 -0500

    update utils

--- src/utils/config.py
+++ src/utils/config.py
@@ -5,14 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    'type': os.getenv('DB_TYPE', 'postgresql'),
-    'user': os.getenv('DB_INGEST_USER', 'DB_INGEST_USER'),
-    'password': os.getenv('DB_INGEST_PASSWORD', 'DB_INGEST_PASSWORD'),
-    'host': os.getenv('DB_HOST', 'localhost'),
-    'port': os.getenv('DB_PORT', '5432'),
-    'database': os.getenv('DB_NAME', 'zillow_analytics')
+    'type': os.getenv('DB_TYPE'),
+    'user': os.getenv('DB_INGEST_USER'),
+    'password': os.getenv('DB_INGEST_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),
+    'database': os.getenv('DB_NAME')
 }
 
 API_CONFIG = {
     
 }
+
