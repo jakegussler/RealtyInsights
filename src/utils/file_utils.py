b4756e1cdf868af99444c419a71af91287bf93fe
Jake Gussler <jakegussler@gmail.com>
2025-01-11 12:17:25 -0500

    add write response to csv to file utils

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -3,8 +3,16 @@ from utils.logger_utils import setup_logging
 
 logger = setup_logging()
 
-def create_folderpath_if_not_exists(folderpath):
+
+def create_output_dir(folderpath):
 
     if not os.path.exists(folderpath):
         os.makedirs(folderpath)
         logger.info(f"Created directory: {folderpath}")
+
+def write_response_to_csv(response, filepath):
+
+    with open(filepath, "wb") as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                file.write(chunk)
