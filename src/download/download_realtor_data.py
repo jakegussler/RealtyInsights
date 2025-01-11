ffc45857d7ad22fbc1677955c482b90988235c85
Jake Gussler <jakegussler@gmail.com>
2025-01-11 12:18:31 -0500

    move functions to utils

--- src/download/download_realtor_data.py
+++ src/download/download_realtor_data.py
@@ -4,7 +4,8 @@ import datetime
 import time
 from utils.logger_utils import setup_logging
 import requests
-from utils.file_utils import create_output_dir
+from utils.file_utils import create_output_dir, write_response_to_csv
+from utils.api_utils import get_response
 
 
 #Load the environment variables
@@ -54,45 +55,6 @@ def get_csv(url: str, csv_output_name: str, folderpath: str) -> None:
         logger.error(f"Error getting response for {csv_output_name}: {e}")
 
 
-
-def write_response_to_csv(response, filepath):
-
-    with open(filepath, "wb") as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            if chunk:
-                file.write(chunk)
-
-def get_response(url, url_parameters=None, max_retries=10, retry_delay=5, timeout=30):
-
-    #Initial retry delay
-    initial_retry_delay = retry_delay
-
-    #Retry loop
-    for attempt in range(max_retries):
-        try:
-            #Make the request to the API
-            response = requests.get(url, params=url_parameters, timeout=timeout)
-            response.raise_for_status() #Raise an error for bad HTTP responses
-            #Exit attempt loop if successful
-            if response.status_code == 200:
-                #Reset the retry delay
-                retry_delay = initial_retry_delay
-                return response
-        
-        except requests.exceptions.RequestException as e:
-            logger.info(f'Attempt {attempt + 1} of {max_retries} failed: {str(e)}')
-            if attempt < max_retries:
-                logger.info(f"Retrying in {retry_delay} seconds...")
-                
-                #Exponential backoff
-                if attempt > 0:
-                    retry_delay = round(retry_delay * 1.5, 0)
-                
-                #Wait for the retry delay
-                time.sleep(retry_delay)
-            else:
-                print("Max retries reached. Returning None")
-                return None
 
 def download_all_realtor_csv_files(folderpath: str) -> None:
     
