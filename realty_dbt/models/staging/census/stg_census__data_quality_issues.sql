1a48dc537c715ae8dba98bc0cb6a7fff20d70bac
Jake Gussler <jakegussler@gmail.com>
2025-02-07 21:38:23 -0500

    data quality issues as select

--- realty_dbt/models/staging/census/stg_census__data_quality_issues.sql
+++ realty_dbt/models/staging/census/stg_census__data_quality_issues.sql
@@ -1,25 +1,27 @@
--- models/staging/stg_data_quality_issues.sql
+-- models/staging/census/stg_census__data_quality_issues.sql
 {{ config(
     materialized='table',
-    indexes=[
-        {'columns': ['source_system', 'source_table']},
-        {'columns': ['detection_timestamp']},
-        {'columns': ['resolution_status']},
-        {'columns': ['key_columns'], 'type': 'gin'}
-    ]
+    schema='stg_census'
 ) }}
 
-CREATE TABLE IF NOT EXISTS {{ this }} (
-    issue_id SERIAL PRIMARY KEY,
-    source_system TEXT NOT NULL,
-    source_table TEXT NOT NULL,
-    key_columns JSONB NOT NULL,
-    column_name TEXT NOT NULL,
-    issue_type TEXT NOT NULL,
-    issue_description TEXT,
-    original_value TEXT,
-    cleaned_value TEXT,
-    detection_timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    impact_description TEXT,
-    notes TEXT
+WITH empty_dq_table AS (
+    SELECT
+        cast(null as bigint) as issue_id,
+        cast(null as text) as source_system,
+        cast(null as text) as source_table,
+        cast(null as jsonb) as key_columns,
+        cast(null as text) as column_name,
+        cast(null as text) as issue_type,
+        cast(null as text) as issue_description,
+        cast(null as text) as original_value,
+        cast(null as text) as cleaned_value,
+        cast(null as timestamptz) as detection_timestamp,
+        cast(null as text) as impact_description,
+        cast(null as text) as notes
+    WHERE false
 )
+
+SELECT 
+    {{ dbt_utils.surrogate_key(['source_system', 'source_table', 'column_name', 'detection_timestamp']) }} as issue_id,
+    *
+FROM empty_dq_table
