110d533f52b93547781dc236797a6981f388a1e9
Jake Gussler <jakegussler@gmail.com>
2025-02-07 20:57:31 -0500

    set up logging

--- realty_dbt/macros/clean_census_value.sql
+++ realty_dbt/macros/clean_census_value.sql
@@ -1,6 +1,6 @@
 {% macro clean_census_value(col, col_type) %}
-    case when {{ col }} in (select value_code from {{ ref('stg_value_annotations') }} where value_code is not null) 
+    case when {{ col }} in (select value_code from {{ ref('census__value_annotations') }} where value_code is not null) 
          then null 
-         else  cast {{ col }} as {{ col_type }}
-    end as {{ col }}
+         else  cast({{ col }} as {{ col_type }})
+    end
 {% endmacro %}
--- realty_dbt/macros/log_quality_issues_census.sql
+++ realty_dbt/macros/log_quality_issues_census.sql
@@ -1,7 +1,6 @@
--- macros/data_quality/log_quality_issues.sql
 {% macro log_quality_issues_census(source_table, columns) %}
 
-insert into {{ ref('stg_data_quality_issues') }} (
+insert into {{ ref('stg_census__data_quality_issues') }} (
     source,
     source_table,
     key_columns,
@@ -23,24 +22,17 @@ select
     va.issue_type,
     va.issue_description as issue_description,
     col.value::text as original_value,
-    'dbt_transformation' as detection_method,
-    case 
-        when va.issue_type in ('insufficient_sample', 'insufficient_cases') then 'high'
-        when va.issue_type in ('not_available', 'missing_data') then 'medium'
-        else 'low'
-    end as severity
 from {{ source_table }} src
 cross join lateral (
     values
     {% for column in columns %}
         ('{{ column }}', src.{{ column }}){% if not loop.last %},{% endif %}
-    {% endfor %}
+{% endfor %}
 ) as col(column_name, value)
-inner join {{ ref('stg_value_annotations') }} va
+inner join {{ ref('census__value_annotations') }} va
     on col.value = va.value_code
-    or (col.value is null and va.value_code is null)
 where col.value in (
-    select value_code from {{ ref('stg_value_annotations') }}
+    select value_code from {{ ref('census__value_annotations') }}
     where value_code is not null
 )
 or col.value is null
