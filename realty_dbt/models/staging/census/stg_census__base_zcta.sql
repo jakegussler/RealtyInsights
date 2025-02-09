9b223a4bb42d8017864c0b1b4fbcd32a32040064
Jake Gussler <jakegussler@gmail.com>
2025-02-08 23:50:00 -0500

    census staing views

---
+++ realty_dbt/models/staging/census/stg_census__base_zcta.sql
@@ -0,0 +1,42 @@
+{{ config(
+    materialized='view'
+) }}
+
+
+
+{% set census_variables = {
+    'median_rent_estimate': 'NUMERIC(18,2)',
+    'median_rent_moe': 'NUMERIC(18,2)',
+    'median_rent_studio_estimate': 'NUMERIC(18,2)',
+    'median_rent_studio_moe': 'NUMERIC(18,2)',
+    'median_rent_1_bedroom_estimate': 'NUMERIC(18,2)',
+    'median_rent_1_bedroom_moe': 'NUMERIC(18,2)',
+    'median_rent_2_bedroom_estimate': 'NUMERIC(18,2)',
+    'median_rent_2_bedroom_moe': 'NUMERIC(18,2)',
+    'median_rent_3_bedroom_estimate': 'NUMERIC(18,2)',
+    'median_rent_3_bedroom_moe': 'NUMERIC(18,2)',
+    'median_rent_4_bedroom_estimate': 'NUMERIC(18,2)',
+    'median_rent_4_bedroom_moe': 'NUMERIC(18,2)',
+    'median_rent_5_bedroom_estimate': 'NUMERIC(18,2)',
+    'median_rent_5_bedroom_moe': 'NUMERIC(18,2)',
+    'residences_total_estimate': 'INTEGER',
+    'residences_total_moe': 'INTEGER',
+    'residences_studio_estimate': 'INTEGER',
+    'residences_studio_moe': 'INTEGER',
+    'residences_1_bedroom_estimate': 'INTEGER',
+    'residences_1_bedroom_moe': 'INTEGER',
+    'residences_2_bedroom_estimate': 'INTEGER',
+    'residences_2_bedroom_moe': 'INTEGER',
+    'residences_3_bedroom_estimate': 'INTEGER',
+    'residences_3_bedroom_moe': 'INTEGER',
+    'residences_4_bedroom_estimate': 'INTEGER',
+    'residences_4_bedroom_moe': 'INTEGER',
+    'residences_5_bedroom_estimate': 'INTEGER',
+    'residences_5_bedroom_moe': 'INTEGER'
+} %}
+
+{% set extra_columns = [
+    {"source": '"zip code tabulation area"', "alias": "zcta"}
+] %}
+
+{{ build_census_staging(source('census', 'census_acs5_base_zcta'), extra_columns, census_variables) }}
