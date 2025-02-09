3ebd583669da1feff8c7fa79fa182fd8c3e6f4de
Jake Gussler <jakegussler@gmail.com>
2025-02-08 22:22:30 -0500

    create census staging model

--- realty_dbt/models/staging/census/stg_census__subject_zcta.sql
+++ realty_dbt/models/staging/census/stg_census__subject_zcta.sql
@@ -1,8 +1,7 @@
-{% set census_variables = get_census_subject_variables() %}
 {{
-    config(
-        materialized='view',
-    )
+  config(
+    materialized='view',
+  )
 }}
 
 {% set census_variables = {
@@ -14,15 +13,8 @@
     'population_below_poverty_level_under_18_moe': 'INTEGER'
 } %}
 
-with cleaned_data as (
-    SELECT
-        name::TEXT AS name,
-        geo_id::TEXT AS geo_id,
-        "zip code tabulation area"::TEXT AS zcta,
-        year::INTEGER AS year,
-        {% for col, col_type in census_variables.items() %}
-            {{ clean_census_value(col, col_type) }} AS {{ col }}{% if not loop.last %},{% endif %}
-        {% endfor %}
-    FROM {{ source('census', 'census_acs5_subject_zcta') }}
-)
-select * from cleaned_data
+{% set extra_columns = [
+    {"source": '"zip code tabulation area"', "alias": "zcta"}
+] %}
+
+select * from {{ build_census_view(source('census', 'census_acs5_subject_zcta'), extra_columns, census_variables) }}
