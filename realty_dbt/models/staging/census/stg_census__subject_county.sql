4f2baa0faafe67d075cb2a80c18e1d54c9f064ba
Jake Gussler <jakegussler@gmail.com>
2025-02-08 22:22:49 -0500

    create subject county and us

---
+++ realty_dbt/models/staging/census/stg_census__subject_county.sql
@@ -0,0 +1,20 @@
+{{
+  config(
+    materialized='view',
+  )
+}}
+
+{% set census_variables = {
+    'median_household_income_estimate': 'NUMERIC(18,2)',
+    'median_household_income_moe': 'NUMERIC(18,2)',
+    'population_below_poverty_level_estimate': 'NUMERIC(18,2)',
+    'population_below_poverty_level_moe': 'NUMERIC(18,2)',
+    'population_below_poverty_level_under_18_estimate': 'INTEGER',
+    'population_below_poverty_level_under_18_moe': 'INTEGER'
+} %}
+
+{% set extra_columns = [
+    {"source": "county", "alias": "county"}
+] %}
+
+select * from {{ build_census_view(source('census', 'census_acs5_subject_county'), extra_columns, census_variables) }}
